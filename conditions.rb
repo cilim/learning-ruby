c9430e810e78e367121768b65e2fe00c1eaffad3
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-15 14:27:24 +0200

    Updates conditions with new type of comparison of 2 numbers. Pretty important

--- conditions.rb
+++ conditions.rb
@@ -27,3 +27,17 @@ puts "You are younger than 10" unless age >= 10
 age = 5
 
 puts "You are younger than 10" unless age >= 10
+
+#Comparison to see which is greater than which
+
+x = 10
+
+y = 20
+
+if (x <=> y) == 0
+	puts "They are the same"
+elsif (x <=> y) == 1
+	puts "X is greater"
+else #if the second number is greater it returns -1
+	puts "y is greater"
+end
