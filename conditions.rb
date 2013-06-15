525536a3a08f9ee69a26deb25aabdc3ffb8b9420
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-15 14:22:13 +0200

    Shows how conditions are used in Ruby in the new .rb file called conditions

---
+++ conditions.rb
@@ -0,0 +1,29 @@
+#In here we can see how Ruby simplifies the use of conditions
+
+
+
+age = 14
+
+if (age == 14)
+	puts "you are 14 years old"
+end
+
+#this is exactly the same as this
+
+puts "you are 14 years old" if age == 14
+
+#Now to see more complex conditions
+
+puts "You are between 10 and 20" if age > 10 && age < 20
+
+#is the same as
+
+puts "You are between 10 and 20" if age.between?(10, 20)
+
+#there is also the unless condition
+
+puts "You are younger than 10" unless age >= 10
+
+age = 5
+
+puts "You are younger than 10" unless age >= 10
