2b0a66f48608f660fa7de696d40bd674571af07f
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-16 15:44:44 +0200

    Shows addition, subtraction and concatination of elements in separate arrays

--- arrays.rb
+++ arrays.rb
@@ -27,3 +27,18 @@ puts "This is a pretty long sentence that contains a lot of words".split(/\s+/).
 puts "In this example we split a whole chapter into sentences. For example, the previous sentence is going to be in the same array but with an other index. As well as this one".split(/\./).inspect
 
 p "The previous example is the same as this one, but here we are splitting parts of a sentences separated with a comma, and we are using an alternative to ispect, and puts".split(/\,/)
+
+#Array addition, subtraction and concatenation
+
+a = [1,2,3,4,5]
+b = [1,2,4,5,6]
+p a
+p b
+
+c = a + b
+p c
+c = a - b
+p c
+d = ["a", "b", "c"]
+e = c + d
+p e
