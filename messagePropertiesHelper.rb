ff827d0f834ccb5b359cb67142d04b8e019ddb7e
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-17 22:42:22 +0200

    Removes old files

--- messagePropertiesHelper.rb
+++
@@ -1,4 +0,0 @@
-text = File.readlines(ARGV[0])
-languages = text.join.split(/\n/)
-
-languages.each {|language| puts "Language." + language.upcase.to_s + "=" + language}
--- part I/textAnalyzer/argv.rb
+++
@@ -1,3 +0,0 @@
-puts ARGV.join("-")
-#If you run this simple script you wont get anything if you just do: ruby argv.rb
-#but if you run it like: ruby argv.rb test 123, the output will be test-123
