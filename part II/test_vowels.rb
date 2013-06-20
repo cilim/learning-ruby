f5e322c3c7a0a8ceb4b06d1bdb53bcd78d9ae983
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-20 12:58:50 +0200

    More differences between load and requuire

--- part II/test_vowels.rb
+++ part II/test_vowels.rb
@@ -4,3 +4,15 @@ puts "This is a test".vowels.join('-')
 
 #you can also use require instead of load (NOT include...its not working with include)
 #if using require, you dont need the .rb extension, but if you use load, then you need it
+
+#IMPORTANT: when including a file load always processes the code upon including it. require doesnt do that
+
+#for example
+#a.rb outputs Hello
+#you are in b.rb which outputs World
+#load a.rb
+#puts "World"
+#load a.rb
+#output: hello world hello
+
+#if you used require, hello would be output only the first time
