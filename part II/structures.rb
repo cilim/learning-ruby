2ce2bf95f98bc6e1b0f9052fca72a6a6008498f9
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-22 01:32:05 +0200

    Structures using open struct gem

--- part II/structures.rb
+++ part II/structures.rb
@@ -4,3 +4,16 @@ Person = Struct.new(:name, :age)
 dummy = Person.new("Marko", 35)
 dummy.age += 1
 puts dummy.age
+
+#you can use structures with the openstruct module (gem) which is more easy, but the code gets harder to read
+
+
+require 'ostruct'
+
+animal = OpenStruct.new
+animal.name = "Fred"
+animal.age = 23
+animal.size = "HUGE"
+puts animal.name
+puts animal.age
+puts animal.size
