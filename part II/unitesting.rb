6a62f78a3a10545e74447a097ddd59a82da8bfb1
Marko Cilimkovic <os.chilim@gmail.com>
2013-06-26 15:15:09 +0200

    Adds more usefull commands for unitesting

--- part II/unitesting.rb
+++ part II/unitesting.rb
@@ -15,3 +15,20 @@ class TestTitleize < Test::Unit::TestCase
 end
 
 #to run this you need to sudo gem install test-unit
+
+#	 assert(<boolean expression>): Passes only if the Boolean expression isn’t false or nil
+#(for example, assert 2 == 1 will always fail).
+#	 assert_equal(expected, actual): Passes only if the expected and actual values are
+#equal (as compared with the == operator). assert_equal('A', 'a'.upcase) will pass.
+#	 assert_not_equal(expected, actual): The opposite of assert_equal. This test will fail
+#if the expected and actual values are equal.
+#	 assert_raise(exception_type, ..) { <code block> }: Passes only if the code block
+#following the assertion raises an exception of the type(s) passed as arguments.
+#assert_raise (ZeroDivisionError) { 2 / 0 } will pass.
+#	 assert_nothing_raised(exception_type, ..) { }: The opposite of assert_raise.
+#Passes only if none of the exceptions listed are raised.
+#	 assert_instance_of(class_expected, object): Passes only if object is of class
+#class_expected.
+#	 flunk: flunk is a special type of assertion in that it will always fail. It’s useful if you
+#haven’t quite finished writing your tests and you want to add a strong reminder that
+#your test case isn’t complete!
